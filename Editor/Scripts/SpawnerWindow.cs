20dd530776e7d79caec59c335e5cf8378c16a11f
CircusCharlie24 <divkaransingh@gmail.com>
2023-09-09 13:19:17 +0530

    Adding asmdef

--- Editor/Scripts/SpawnerWindow.cs
+++ Editor/Scripts/SpawnerWindow.cs
@@ -2,22 +2,27 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class SpawnerWindow : EditorWindow
+namespace CodeLibrary24.PointToPointSpawner
 {
-    [SerializeField]
-    private VisualTreeAsset m_VisualTreeAsset = default;
-
-    [MenuItem("CodeLibrary24/PointToPointSpawner/SpawnerWindow")]
-    public static void ShowWindow()
+    public class SpawnerWindow : EditorWindow
     {
-        SpawnerWindow wnd = GetWindow<SpawnerWindow>();
-        wnd.titleContent = new GUIContent("SpawnerWindow");
+        [SerializeField]
+        private VisualTreeAsset m_VisualTreeAsset = default;
+
+        [MenuItem("CodeLibrary24/PointToPointSpawner/SpawnerWindow")]
+        public static void ShowWindow()
+        {
+            SpawnerWindow wnd = GetWindow<SpawnerWindow>();
+            wnd.titleContent = new GUIContent("SpawnerWindow");
+        }
+
+        public void CreateGUI()
+        {
+            VisualElement root = rootVisualElement;
+            VisualElement container = m_VisualTreeAsset.Instantiate();
+            root.Add(container);
+
+        }
     }
 
-    public void CreateGUI()
-    {
-        VisualElement root = rootVisualElement;
-        VisualElement container = m_VisualTreeAsset.Instantiate();
-        root.Add(container);
-    }
 }
